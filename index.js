f02330fb0f7d15bd6d80a823909991b7ea0a33ab
May Nir <maynirrr@gmail.com>
2023-04-29 19:52:14 +0300

    entry

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const fs = require('fs');
 const app = express()
 const port = 3000
 
@@ -10,6 +11,30 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/entry', (req, res) => {
+  const plate = req.query.plate;
+  const parkingLot = req.query.parkingLot;
+
+  const ticketIdCounter = JSON.parse(fs.readFileSync('db/ticket_id_counter.json')).id;
+  const ticketId = ++ticketIdCounter;
+
+  const timeOfArrival = Date.now();
+
+  const entry = {
+    plate: plate,
+    parkingLot: parkingLot,
+    timeOfArrival: timeOfArrival,
+  };
+
+  const database = JSON.parse(fs.readFileSync('db/parking_lot_entries.json'));
+  database[ticketId] = entry;
+  fs.writeFileSync('db/parking_lot_entries.json', JSON.stringify(database));
+
+  fs.writeFileSync('db/ticket_id_counter.json', JSON.stringify({ id: ticketIdCounter })); // update ticket id
+
+  res.json({ ticketId: ticketId });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
